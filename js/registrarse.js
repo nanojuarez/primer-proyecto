ec5f654e2d23de65a9684c0fc74bcd794cbb93e1
Victor Fernando Juarez <victorfernandojurez@gmail.com>
2023-03-07 15:16:59 -0300

    cambio en mi primer proyecto

---
+++ js/registrarse.js
@@ -0,0 +1,7 @@
+const nombre = document.getElementById("miNombre");
+const apellido = document.getElementById("miApellido");
+const mail = document.getElementById("miMail");
+const celular = document.getElementById("miCelular");
+const pass = document.getElementById("miPassword");
+const rpass = document.getElementById("miRpassword");
+
